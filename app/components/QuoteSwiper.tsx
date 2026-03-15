9fd6cfbcf5b45b8b1c47033e7d6d63a2270f7ddb
John Salvador <cpcount1@gmail.com>
2026-03-15 23:49:05 +0800

    Update bug
    Update bug

--- app/components/QuoteSwiper.tsx
+++ app/components/QuoteSwiper.tsx
@@ -33,7 +33,7 @@ export default function QuoteSwiper() {
 		}
 	}
 
-	async function fetchRandomVerse(bookList, bible_translation) {
+	async function fetchRandomVerse(bookList: string[], bible_translation: string) {
 		setLoading(true);
 		if (!bookList || bookList.length === 0) return;
 
